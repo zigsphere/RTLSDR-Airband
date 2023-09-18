8c0f8980de2b3fbd9a6a72ccead5eb7ec5505d2a
TheGreatCodeHolio <ian@icarey.net>
2023-09-17 22:39:52 -0400

    Added run script on file completion.
    New config settings in channel
    external_script : default "" : Path to a external script to be run on completion of MP3 file creation
    
    Script runs in a thread, threads are limited to 4 at a time. Threads also have a time out of 30 seconds.

--- src/config.cpp
+++ src/config.cpp
@@ -195,6 +195,42 @@ static int parse_outputs(libconfig::Setting &outs, channel_t *channel, int i, in
                 (bool)(outs[o]["include_freq"]) : false;
             channel->needs_raw_iq = channel->has_iq_outputs = 1;
 
+            if(outs[o].exists("external_script"))
+            {
+                fdata->external_script = strdup(outs[o]["external_script"].c_str());
+            }
+            else
+            {
+                fdata->external_script = strdup("");
+            }
+
+            if(outs[o].exists("transmission_delay"))
+            {
+                fdata->max_transmission_idle_sec = outs[o]["transmission_delay"];
+            }
+            else
+            {
+                fdata->max_transmission_idle_sec = 0.5;
+            }
+
+            if(outs[o].exists("minimum_transmission"))
+            {
+                fdata->min_transmission_time_sec = outs[o]["minimum_transmission"];
+            }
+            else
+            {
+                fdata->min_transmission_time_sec = 1.0;
+            }
+
+            if(outs[o].exists("max_transmission") && fdata->split_on_transmission)
+            {
+                fdata->max_transmission_time_sec = outs[o]["max_transmission"];
+            }
+            else
+            {
+                fdata->max_transmission_time_sec = 3600;
+            }
+
             if(fdata->continuous && fdata->split_on_transmission) {
                 cerr<<"Configuration error: devices.["<<i<<"] channels.["<<j<<"] outputs.["<<o<<"]: can't have both continuous and split_on_transmission\n";
                 error();
